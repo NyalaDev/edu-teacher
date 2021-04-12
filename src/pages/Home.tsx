a88eba0645f3894b47a4a4e9f121b8426b44a7fd
Alaa Yahia <alaay873@live.com>
2021-04-12 12:08:50 +0200

    Teacher dashboard: implement sorting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { orderBy } from 'lodash';
 import { CourseCard } from '../components/courses';
 import { ActivityIndicator, TextInput } from '../components/UI';
-
 import { getCourses } from '../services/api.service';
 import { Course } from '../types/api.types';
 
@@ -11,7 +11,13 @@ const Home: React.FC = () => {
   const { t } = useTranslation();
   const [fetching, setFetching] = useState(true);
   const [apiCourses, setApiCourses] = useState<[Course]>();
-  const [courses, setCourses] = useState<[Course]>();
+  const [courses, setCourses] = useState<Course[]>();
+  const [statusValue, setStatusValue] = useState({
+    status: '-1',
+  });
+  const [createdAtValue, setCreatedAtValue] = useState({
+    created_at: '-1',
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,6 +62,40 @@ const Home: React.FC = () => {
     }
   };
 
+  const onSortbyCreationDate = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const {
+      target: { value },
+    } = e;
+
+    if (!value) {
+      return;
+    }
+    const sortType = value === 'newest' ? 'desc' : 'asc';
+    const sortedCourses = orderBy(courses, 'created_at', sortType);
+    setCreatedAtValue({ created_at: value });
+    setStatusValue({ status: '-1' });
+    setCourses(sortedCourses);
+  };
+
+  const onSortbystatus = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const {
+      target: { value },
+    } = e;
+    if (!value) {
+      return;
+    }
+    setStatusValue({ status: value });
+    const sortType = value === 'draft' ? 'asc' : 'desc';
+    const sortedCourses = orderBy(
+      courses,
+      [course => course.status.toLowerCase()],
+      [sortType]
+    );
+    setStatusValue({ status: value });
+    setCreatedAtValue({ created_at: '-1' });
+    setCourses(sortedCourses);
+  };
+
   return (
     <ActivityIndicator active={fetching}>
       <h1 className="text-4xl font-bold leading-tight mb-5">{t('courses')}</h1>
@@ -65,6 +105,28 @@ const Home: React.FC = () => {
             placeholder={t('filterCourses')}
             onChange={onFilterCourse}
           />
+          <span>Sort by</span>
+          <select
+            className="w-full py-2 px-4 bg-gray-100 text-gray-700 border border-gray-300 rounded  block appearance-none placeholder-gray-500 focus:outline-none focus:bg-white"
+            onChange={onSortbyCreationDate}
+            value={createdAtValue.created_at}
+            name="created_at"
+          >
+            <option value="-1">{t('creationDate')}</option>
+            <option value="newest">{t('newest')}</option>
+            <option value="oldest">{t('oldest')}</option>
+          </select>
+
+          <select
+            className="w-full py-2 px-4 bg-gray-100 text-gray-700 border border-gray-300 rounded  block appearance-none placeholder-gray-500 focus:outline-none focus:bg-white"
+            onChange={onSortbystatus}
+            value={statusValue.status}
+            name="status"
+          >
+            <option value="-1">{t('status')}</option>
+            <option value="published">{t('published')}</option>
+            <option value="draft">{t('draft')}</option>
+          </select>
         </div>
         <div>
           <Link
